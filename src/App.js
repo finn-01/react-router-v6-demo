f5958011cceda7b2ffcfbb46a648052e5dcc9b0d
Nguyen Van Sam <53646493+finn-01@users.noreply.github.com>
2021-12-22 23:43:47 +0700

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import { Routes, Route, Link } from "react-router-dom";
+
+import "./styles.css";
+import HomePage from "./pages/Home";
+import NewsPage from "./pages/News";
+import ContactPage from "./pages/Contact";
+
+export default function App() {
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/news">News</Link>
+          </li>
+          <li>
+            <Link to="/contact">Contact</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/news" element={<NewsPage />}></Route>
+        <Route path="/contact" element={<ContactPage />}></Route>
+      </Routes>
+    </div>
+  );
+}
